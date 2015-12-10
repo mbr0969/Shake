249aa546409d11fc35313747bb8fbae3a9164330
mbr0969 <mbr0969@gmail.com>
2015-12-10 09:49:36 +0300

    correct

--- Program.cs
+++ Program.cs
@@ -7,17 +7,9 @@ namespace Shake
 		{
 
 			Point p1 = new Point (4,4,'*');
-
-			//p1.x = 4;
-			//p1.y = 4;
-			//p1.sym = '*';
 			p1.Draw ();
 
-			Point p2 = new Point ();
-
-			p2.x = 7;
-			p2.y = 8;
-			p2.sym = '#';
+			Point p2 = new Point (8,7,'#');
 			p2.Draw ();
 
 			Console.WriteLine ();
